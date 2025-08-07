29bdc5acca8fecbd40c3ab558667f3dcc63694ae
Bravo9000 <renatokrs@gmail.com>
2025-08-07 01:27:35 -0300

    fix(workflow): Correção do changelog.

--- scripts/release/release-changelog.js
+++ scripts/release/release-changelog.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const changelogPath = path.join(__dirname, '../../CHANGELOG.md');
 const version = process.argv[2];
 
